2e0858f38a34a8b6c8d8c421caec9620cfbf1a38
Simon Belt <82307311+Ginlick@users.noreply.github.com>
2022-12-25 18:52:08 +0100

    First call to billetweb

---
+++ getdates.js
@@ -0,0 +1,10 @@
+
+//billetweb
+var url = "https://www.billetweb.fr/api/events?user=153926&key=c4ca4238a0b923820dcc509a6f75849b&version=1"; //this is identification for my own billetweb account (Zamies Test)
+var xmlHttp = new XMLHttpRequest();
+xmlHttp.onreadystatechange = function() {
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+        console.log(xmlHttp.responseText); //should be a json list of event dictionaries
+}
+xmlHttp.open("GET", url, true); // true for asynchronous
+xmlHttp.send(null);
